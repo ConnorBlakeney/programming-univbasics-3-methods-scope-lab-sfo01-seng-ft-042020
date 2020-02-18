75114c52fb438fe2ff5cbb1061ba679258dca16f
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-18 02:20:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,9 +13,7 @@ def link
   puts phrase
 end
 
-describe "any_phrase" do
-  it "takes in an argument and puts out the catchphrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
+def any_phrase
+  phrase = "Do a barrell roll!"
+  puts phrase
 end
